722cc35cdbbe1cd8621f08c85961876507a473db
seonghyeoklee <dltjdgur327@gmail.com>
2022-08-19 23:57:11 +0900

    init solog project

---
+++ src/main/java/com/solog/SologApplication.java
@@ -0,0 +1,13 @@
+package com.solog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SologApplication.class, args);
+    }
+
+}
