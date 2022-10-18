bab4af435d1109ae074383d1f14075f47293a589
seonghyeoklee <dltjdgur327@gmail.com>
2022-10-18 22:46:52 +0900

    feat : Spring REST Docs 꾸미기 - 3

--- src/test/java/com/solog/api/controller/PostControllerTest.java
+++ src/test/java/com/solog/api/controller/PostControllerTest.java
@@ -1,13 +1,5 @@
 package com.solog.api.controller;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solog.api.request.PostDto.PostCreate;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
@@ -43,11 +43,11 @@ class PostControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(body))
                 )
+                .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.status").value("400"))
                 .andExpect(jsonPath("$.message").value("Required parameter"))
                 .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
-                .andDo(print())
                 .andDo(
                         document("posts",
                                 responseFields(
@@ -55,11 +55,10 @@ class PostControllerTest {
                                         fieldWithPath("data").description("Post 제목"),
                                         fieldWithPath("error").description("Post 내용"),
                                         fieldWithPath("message").description("Post 내용"),
-                                        fieldWithPath("validation.title").description("Post 내용")
+                                        fieldWithPath("validation.title").description("Post 타이틀")
                                 )
                         )
                 );
-
     }
 
     @Test
@@ -71,16 +70,27 @@ class PostControllerTest {
             .build();
 
         mockMvc.perform(
-                post("/posts")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(body))
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value("400"))
-            .andExpect(jsonPath("$.message").value("Required parameter"))
-            .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
-            .andExpect(jsonPath("$.validation.content").value("컨텐츠을 입력해주세요."))
-            .andDo(print());
+                        post("/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(body))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+                .andExpect(jsonPath("$.message").value("Required parameter"))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+                .andExpect(jsonPath("$.validation.content").value("컨텐츠을 입력해주세요."))
+                .andDo(
+                        document("posts-not-valid",
+                                responseFields(
+                                        fieldWithPath("status").description("Post Id"),
+                                        fieldWithPath("data").description("Post 제목"),
+                                        fieldWithPath("error").description("Post 내용"),
+                                        fieldWithPath("message").description("Post 내용"),
+                                        fieldWithPath("validation.title").description("Post 타이틀"),
+                                        fieldWithPath("validation.content").description("Post 내용")
+                                )
+                        )
+                );
     }
 
 }
